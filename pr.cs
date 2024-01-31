d687c49268e82f3e880002e8d07fdb5b5cdc205f
PristanMe <mirongall@yandex.ru>
2024-01-31 10:30:26 +0300

    первая часть

---
+++ pr.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!\n\n");
+            Console.Write("Введите число A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A + B = " + (a + b));
+            Console.WriteLine("A - B = " + (a - b));
+            Console.WriteLine("A * B = " + (a * b));
+            Console.WriteLine("A / B = " + (a / b));
+            Console.ReadKey();
+        }
+    }
+}
